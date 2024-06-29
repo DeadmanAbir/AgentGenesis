acd53c7bc3c8c73e9c024c5da24648505c54fd5c
faisal004 <faisalhusain1320@gmail.com>
2024-06-29 13:21:41 +0530

    fix : more refactoring

--- src/app/(app)/agents/[[...slug]]/page.tsx
+++ src/app/(app)/agents/[[...slug]]/page.tsx
@@ -56,7 +56,7 @@ export async function Agents({ params }: AgentsPageProps) {
             </p>
           )}
         </div>
-        <div className="pb-12 pt-8 prose dark:prose-invert">
+        <div className="pb-12 pt-8 prose dark:prose-invert prose-p:my-0">
           <Mdx code={doc.body.code} />
         </div>
       </div>
--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -4,21 +4,107 @@ import * as React from 'react';
 
 import { useMDXComponent } from 'next-contentlayer/hooks';
 
-import { useConfig } from '@/hooks/use-config';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from './ui/accordion';
+import { cn } from '@/lib/utils';
+
+const components = {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+
+  h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h1
+      className={cn(
+        'font-heading mt-2 scroll-m-20 text-4xl font-bold',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h2
+      className={cn(
+        'font-heading mt-12 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h3
+      className={cn(
+        'font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  h4: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h4
+      className={cn(
+        'font-heading mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  h5: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h5
+      className={cn(
+        'mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  h6: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h6
+      className={cn(
+        'mt-8 scroll-m-20 text-base font-semibold tracking-tight',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
+    <a
+      className={cn('font-medium underline underline-offset-4', className)}
+      {...props}
+    />
+  ),
+  p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
+    <p
+      className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}
+      {...props}
+    />
+  ),
+  ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
+    <ul className={cn('my-6 ml-6 list-disc', className)} {...props} />
+  ),
+  ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
+    <ol className={cn('my-6 ml-6 list-decimal', className)} {...props} />
+  ),
+  li: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+    <li className={cn('mt-2', className)} {...props} />
+  ),
+};
 
 interface MdxProps {
   code: string;
 }
 
 export function Mdx({ code }: MdxProps) {
-  const [config] = useConfig();
-  const Component = useMDXComponent(code, {
-    style: config.style,
-  });
+  const Component = useMDXComponent(code);
 
   return (
     <div className="mdx">
-      <Component />
+      <Component components={components} />
     </div>
   );
 }
