82aaa7e1c21a95321cf5b339281f5c4835a5a4c4
faisal004 <faisalhusain1320@gmail.com>
2024-07-08 21:46:34 +0530

    fix : css fix

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -11,12 +11,12 @@ import useScrollTop from '@/hooks/use-scroll';
 const SiteHeader = () => {
   const scrolled = useScrollTop();
   return (
-    <header className=" w-full border-b border-border/40 fixed top-2 z-40 ">
+    <header className=" w-full  fixed top-2 z-40 ">
       <div
-        className={`container  mx-auto flex h-14 max-w-[1240px] items-center rounded-full transition duration-500 ${scrolled ? 'dark:bg-neutral-900 bg-white ' : ' bg-transparent'}   `}
+        className={`container   mx-auto flex h-14 max-w-[1240px] items-center rounded-full transition duration-500 ${scrolled ? 'dark:bg-neutral-900 bg-white  ' : ' bg-transparent'}   `}
       >
         <div
-          className={`absolute inset-0 h-full w-full max-w-[1240px] mx-auto bg-neutral-100 dark:bg-neutral-800 pointer-events-none [mask-image:linear-gradient(to_bottom,white,transparent,white)] rounded-full transition duration-500  ${scrolled ? 'opacity-100' : 'opacity-0'}`}
+          className={`absolute inset-0 h-full w-full max-w-[1240px] mx-auto bg-[#F9F9F9] dark:bg-neutral-800 pointer-events-none [mask-image:linear-gradient(to_bottom,white,transparent,white)] rounded-full transition duration-500  ${scrolled ? 'opacity-100 border-[2px]' : 'opacity-0'}`}
         ></div>
         <MainNav />
         <MobileNav />
