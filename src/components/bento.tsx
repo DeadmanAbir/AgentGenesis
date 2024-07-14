d5e26687a087dbe95ffdaa808d5ab652b8da2d6c
faisal004 <faisalhusain1320@gmail.com>
2024-07-14 12:51:18 +0530

    changed : bento changed

--- src/components/bento.tsx
+++ src/components/bento.tsx
@@ -1,22 +1,78 @@
+import Image from 'next/image';
+
 const BentoGrid = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center relative py-20 ">
       <div className="flex flex-col items-center justify-center gap-6">
         <div className="md:text-6xl text-4xl text-balance text-center">
           Loved by people all over the universe
         </div>
-        <p className="max-w-[500px] text-center">
+        <p className="max-w-[500px] text-center ">
           Every AI is used by millions of people around the globe.Our APIs have
           fan bases and people fight for us over twitter.
         </p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-6 w-full py-10">
-        <div className="bg-yellow-200 col-span-1 md:col-span-4 h-[500px]">
-          1
+      {/* <div className="border-t-2 border-white overflow-hidden border-dashed  -top-[0px] w-[105%] " ></div> */}
+      <div className="w-full relative mt-10">
+        <div className="grid grid-cols-1 md:grid-cols-6 w-full    ">
+          <div className=" col-span-1 md:col-span-4 h-[500px] p-10  ">
+            <div className="md:text-4xl text-3xl font-semibold">
+              Generate images with text
+            </div>
+            <div className="md:w-1/2 w-full my-3">
+              Generate images from a text prompt, a video, or a video segment in
+              bulk at the speed of light.
+            </div>
+            <div className="w-full h-full  ">
+              <Image
+                src="https://assets.lummi.ai/assets/QmXefBSTibki21yg492eRAxaFvFLc5uu139ghEnUBJRWxd?auto=format&w=1500"
+                alt="iamge"
+                height={200}
+                width={800}
+                className="overflow-hidden object-cover h-[300px] md:p-10 dark:bg-black bg-neutral-500"
+              />
+            </div>
+          </div>
+          <div className="p-5 col-span-1 md:col-span-2 h-[500px] md:border-l-2 border-white/30">
+            <div className="w-full h-full  ">
+              <Image
+                src="https://assets.lummi.ai/assets/QmRMtRgBJzA7wEwbHHasK1AwmVP5cxFhpn7yfVoziwQiYu?auto=format&w=1500"
+                alt="iamge"
+                height={600}
+                width={800}
+                className="overflow-hidden object-cover"
+              />
+            </div>
+          </div>
+          <div className="col-span-1 md:col-span-3 h-[300px]">
+            <div className="w-full h-full  ">
+              <Image
+                src="https://assets.lummi.ai/assets/QmerqxSqpqBcJSs17BVsQbhEARYGbwZL9VGj2x4QDGBJbm?auto=format&w=1500"
+                alt="iamge"
+                height={200}
+                width={800}
+                className="overflow-hidden object-cover  h-[300px]"
+              />
+            </div>
+          </div>
+          <div className=" col-span-1 md:col-span-3 h-[300px]">
+            <div className="w-full h-full  ">
+              <Image
+                src="https://assets.lummi.ai/assets/QmXefBSTibki21yg492eRAxaFvFLc5uu139ghEnUBJRWxd?auto=format&w=1500"
+                alt="iamge"
+                height={200}
+                width={800}
+                className="overflow-hidden object-cover h-[300px] p-10 dark:bg-black bg-neutral-500"
+              />
+            </div>
+          </div>
         </div>
-        <div className="bg-red-200 col-span-1 md:col-span-2 h-[500px]">2</div>
-        <div className="bg-green-200 col-span-1 md:col-span-3 h-[300px]">3</div>
-        <div className="bg-pink-200 col-span-1 md:col-span-3 h-[300px]">4</div>
+        <div className="border-t-2 dark:border-white border-slate-500 overflow-hidden border-dashed absolute md:block hidden -left-4 -top-[2px] w-[103%] "></div>
+
+        <div className="border-l-2 dark:border-white border-slate-500 overflow-hidden border-dashed absolute md:block hidden -right-0.5 -top-[17px] h-[105%] "></div>
+        <div className="border-l-2 dark:border-white border-slate-500 overflow-hidden border-dashed absolute md:block hidden -left-0.5 -top-[17px] h-[105%] "></div>
+
+        <div className="border-t-2 dark:border-white border-slate-500 overflow-hidden border-dashed absolute md:block hidden -left-4 top-[800px] w-[103%] "></div>
       </div>
     </div>
   );
