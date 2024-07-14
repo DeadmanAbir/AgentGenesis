a7f7c5927902b156abe9b984296b13e67191aba4
faisal004 <faisalhusain1320@gmail.com>
2024-07-14 15:04:30 +0530

    added : badge added

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -15,6 +15,7 @@ import TestimonialSection from '@/components/testimonial-section';
 import { buttonVariants } from '@/components/ui/button';
 import { siteConfig } from '@/config/site';
 import { cn } from '@/lib/utils';
+import { Star } from 'lucide-react';
 import Link from 'next/link';
 
 const IndexPage = () => {
@@ -23,6 +24,14 @@ const IndexPage = () => {
       <PageHeader className=" relative z-30  ">
         <Beam />
         <div className="absolute inset-0 -z-10 h-full w-full  dark:bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] bg-[radial-gradient(#000000_1px,transparent_1px)] [background-size:30px_30px]  [mask-image:radial-gradient(ellipse_80%_50%_at_10%_0%,#000_20%,transparent_200%)]"></div>
+        <div className="py-1 text-sm px-5 bg-neutral-700 rounded-full text-white flex items-center gap-2 group  hover:shadow-yellow-500 dark:shadow-slate-500 shadow-slate-300 cursor-pointer shadow-[0_5px_50px_rgba(8,_112,_184,_0.7)]">
+          {' '}
+          <p>We are Open-source. Star Us</p>{' '}
+          <Star
+            className="h-4 w-4 group-hover:scale-125 transition duration-300"
+            fill="yellow"
+          />
+        </div>
         <PageHeaderHeading className="z-20">
           Generate Images, Text and Videos with AI
         </PageHeaderHeading>
