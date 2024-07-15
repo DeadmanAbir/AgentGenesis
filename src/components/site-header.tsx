129d425c341275f02e58307f8afb5a2ab6998ec7
faisal004 <faisalhusain1320@gmail.com>
2024-07-15 22:23:55 +0530

    added : github button added

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -30,10 +30,10 @@ const SiteHeader = () => {
         <div className="flex flex-1 items-center  space-x-2 justify-end">
           {pathname.includes('docs') && (
             <div className="flex items-center">
-              <div className="relative inline-block overflow-hidden rounded-full p-[1px]">
+              <div className="relative group inline-block overflow-hidden rounded-full p-[1px]">
                 <div className="absolute inset-[-1000%] animate-[spin_5s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 <div className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-gray-950 px-3 py-1 text-xs font-medium text-gray-50 backdrop-blur-3xl">
-                  <Github className="h-3 w-4" />
+                  <Github className="h-3 w-4 group-hover:scale-125 duration-200 transition-all" />
                   Star us on Github
                 </div>
               </div>
