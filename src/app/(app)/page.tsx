c593513ec4c8b70bab4937601c71c27eec8edbbe
faisal004 <faisalhusain1320@gmail.com>
2024-07-16 22:32:37 +0530

    changed : bg chamge

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -23,7 +23,7 @@ const IndexPage = () => {
     <>
       <PageHeader className=" relative z-30  ">
         <Beam />
-        <div className="absolute inset-0 -z-10 h-full w-full  dark:bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] bg-[radial-gradient(#000000_1px,transparent_1px)] [background-size:30px_30px]  [mask-image:radial-gradient(ellipse_80%_50%_at_10%_0%,#000_20%,transparent_200%)]"></div>
+        <div className="absolute inset-0 -z-10 h-full w-full  dark:bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] bg-[radial-gradient(#000000_1px,transparent_1px)] [background-size:30px_30px]  [mask-image:radial-gradient(ellipse_80%_50%_at_10%_50%,#000_20%,transparent_130%)]"></div>
         <div className="py-1 text-sm px-5 bg-neutral-700 rounded-full text-white flex items-center gap-2 group  hover:shadow-yellow-500 dark:hover:shadow-yellow-500 dark:shadow-slate-500 shadow-slate-300 cursor-pointer shadow-[0_5px_50px_rgba(8,_112,_184,_0.7)]">
           {' '}
           <Link
