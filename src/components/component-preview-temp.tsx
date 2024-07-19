3ccc3eaf0782ed3986dc4186410f7562a3749c16
faisal004 <faisalhusain1320@gmail.com>
2024-07-19 22:48:52 +0530

    fix : css fix

--- src/components/component-preview-temp.tsx
+++ src/components/component-preview-temp.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/lib/utils';
 
 export function ComponentPreviewTemp({ children }: any) {
   return (
-    <div className={cn('p-20 border-2 rounded-xl mt-5')}>
+    <div className={cn('p-3 border-2 rounded-xl mt-5')}>
       <div>{children}</div>
     </div>
   );
