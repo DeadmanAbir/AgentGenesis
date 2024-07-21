88be38b2ada21c4a8d1d55b4ca12dd2eccc04796
faisal004 <faisalhusain1320@gmail.com>
2024-07-21 11:59:47 +0530

    fix : console.log removed

--- src/components/pre-component.tsx
+++ src/components/pre-component.tsx
@@ -3,7 +3,7 @@ import { CopyButton } from './copy-button';
 
 export const Pre = ({ children, ...props }: any) => {
   const textToCopy = extractTextFromChildren(children);
-  console.log(textToCopy);
+
   return (
     <pre {...props} className="relative">
       <CopyButton text={textToCopy} />
