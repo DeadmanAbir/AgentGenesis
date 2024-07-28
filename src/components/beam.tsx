a156537415e262393cc1ae0d2b89a6963eb857b7
faisal004 <faisalhusain1320@gmail.com>
2024-07-28 13:14:52 +0530

    added : one more beam added

--- src/components/beam.tsx
+++ src/components/beam.tsx
@@ -52,3 +52,54 @@ export const Beam = () => {
     </svg>
   );
 };
+export const Beam2 = () => {
+  return (
+    <svg
+      width="156"
+      height="63"
+      viewBox="0 0 156 63"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="absolute bottom-[69px]  md:left-[600px] -left-60 ml-60 mt-8 rotate-180"
+    >
+      <path
+        d="M40 1h40M0 1h40m40 60h40m-1 0h40m-1 40h40M80 90V0m80 80V40"
+        stroke="url(#grad1)"
+        strokeWidth={2}
+      />
+      <defs>
+        <motion.linearGradient
+          variants={{
+            initial: {
+              x1: '40%',
+              x2: '50%',
+              y1: '160%',
+              y2: '180%',
+            },
+            animate: {
+              x1: '0%',
+              x2: '10%',
+              y1: '-40%',
+              y2: '-20%',
+            },
+          }}
+          animate="animate"
+          initial="initial"
+          transition={{
+            duration: 2.8,
+            repeat: Infinity,
+            repeatType: 'loop',
+            ease: 'linear',
+            repeatDelay: 1,
+          }}
+          id="grad1"
+        >
+          <stop stopColor="#18CCFC" stopOpacity="0" />
+          <stop stopColor="#18CCFC" />
+          <stop offset="0.325" stopColor="#6344F5" />
+          <stop offset="1" stopColor="#AE48FF" stopOpacity="0" />
+        </motion.linearGradient>
+      </defs>
+    </svg>
+  );
+};
