8ad588fc562cd9aeb81227702fd9e94d108b7542
faisal004 <faisalhusain1320@gmail.com>
2024-07-29 20:46:36 +0530

    fix : css fix

--- src/components/DemoComponents/LinkedinAgent.tsx
+++ src/components/DemoComponents/LinkedinAgent.tsx
@@ -175,7 +175,7 @@ const LinkedinAgent: React.FC = () => {
               </PopoverTrigger>
               <PopoverContent
                 side="top"
-                className="flex flex-col items-center gap-3 w-[500px]"
+                className="flex flex-col items-center gap-3 md:w-[500px] w-[300px]"
               >
                 <Select
                   onValueChange={(value) =>
