fcbf85e84c729376a5947f3f0c3e1365b561f094
faisal004 <faisalhusain1320@gmail.com>
2024-08-18 15:19:01 +0530

    added : label added

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -8,6 +8,7 @@ import { siteConfig } from '@/config/site';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
+import { docsConfig } from '@/config/doc';
 
 export function MainNav() {
   const pathname = usePathname();
@@ -34,34 +35,24 @@ export function MainNav() {
           {siteConfig.name}
         </span>
       </Link>
-      <nav className="flex items-center  gap-4 text-sm lg:gap-4  ">
-        <Link
-          href="/docs"
-          className={cn(
-            'transition-colors hover:text-foreground/80 hover:dark:bg-neutral-700 hover:bg-neutral-100  px-2 py-1 rounded-lg',
-            pathname === '/docs' ? 'text-foreground ' : 'text-foreground/60',
-          )}
-        >
-          Docs
-        </Link>
-        <Link
-          href="/pricing"
-          className={cn(
-            'transition-colors hover:text-foreground/80 hover:dark:bg-neutral-700 hover:bg-neutral-100  px-2 py-1 rounded-lg',
-            pathname === '/pricing' ? 'text-foreground ' : 'text-foreground/60',
-          )}
-        >
-          Pricing
-        </Link>
-        <Link
-          href="/blogs"
-          className={cn(
-            'transition-colors hover:text-foreground/80 hover:dark:bg-neutral-700 hover:bg-neutral-100  px-2 py-1 rounded-lg',
-            pathname === '/blogs' ? 'text-foreground ' : 'text-foreground/60',
-          )}
-        >
-          Blogs
-        </Link>
+      <nav className="flex items-center gap-4 text-sm lg:gap-4">
+        {docsConfig.mainNav.map((item: any) => (
+          <Link
+            key={item.href}
+            href={item.href}
+            className={cn(
+              'relative transition-colors hover:text-foreground/80 hover:dark:bg-neutral-700 hover:bg-neutral-100 px-2 py-1 rounded-lg',
+              pathname === item.href ? 'text-foreground' : 'text-foreground/60',
+            )}
+          >
+            {item.title}
+            {item.label && (
+              <span className="ml-2 rounded-md bg-[#FFBD7A] px-1.5 py-0.5 text-xs leading-none text-[#000000] no-underline group-hover:no-underline">
+                {item.label}
+              </span>
+            )}
+          </Link>
+        ))}
       </nav>
     </div>
   );
