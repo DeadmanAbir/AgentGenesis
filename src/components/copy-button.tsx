172f0baca1d15be3167b3ed28b2c948aa1a9dd4a
Faisal Husain <faisalhusain1320@gmail.com>
2025-02-10 07:43:36 +0530

    fix : some css enhancement

--- src/components/copy-button.tsx
+++ src/components/copy-button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { CheckCheck, Copy } from 'lucide-react';
+import { Check, Copy } from 'lucide-react';
 import { useState } from 'react';
 
 export const CopyButton = ({ text }: any) => {
@@ -12,20 +12,34 @@ export const CopyButton = ({ text }: any) => {
 
     setTimeout(() => {
       setIsCopied(false);
-    }, 10000);
+    }, 3000);
   };
 
   return (
     <button
-      className="absolute right-3 top-3"
+      className={`absolute right-3 top-3  p-2 ring-inset ring-2 transition-all duration-200  rounded-[0.3vw] bg-zinc-800 ${
+        isCopied ? 'ring-green-500' : 'ring-zinc-500'
+      } `}
       disabled={isCopied}
       onClick={copy}
     >
-      {isCopied ? (
-        <CheckCheck className="h-4 w-4 text-green-500" />
-      ) : (
-        <Copy className="h-4 w-4 text-white" />
-      )}
+      <div className="relative w-4 h-4">
+        <div
+          className={`absolute inset-0 transform transition-all duration-300 ease-in-out ${
+            isCopied ? 'scale-0 opacity-0' : 'scale-100 opacity-100'
+          }`}
+        >
+          <Copy className="text-white size-4" />
+        </div>
+        <div
+          className={`absolute inset-0 transform transition-all duration-300 ease-in-out ${
+            isCopied ? 'scale-100 opacity-100' : 'scale-0 opacity-0'
+          }
+          `}
+        >
+          <Check className="text-green-500 size-4" />
+        </div>
+      </div>
     </button>
   );
 };
