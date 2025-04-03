be9040c08925dd1df6ffe9a3bbcb8dd674b043c1
Faisal Husain <faisalhusain1320@gmail.com>
2025-04-03 14:55:38 +0530

    add ;  adding effect

--- src/components/linked-in-tool.tsx
+++ src/components/linked-in-tool.tsx
@@ -10,12 +10,14 @@ const LinkedToolEffect = () => {
   const [hasCompleted, setHasCompleted] = useState(false);
   const [callCount, setCallCount] = useState(0);
   const [showGenerate, setShowGenerate] = useState(false);
+  const [showInput, setShowInput] = useState(true);
 
   const runAnimation = async () => {
     await animate('.enter-button', { scale: 0.9 }, { duration: 0.1 });
     await animate('.enter-button', { scale: 1 }, { duration: 0.1 });
     setTimeout(() => {
       setShowGenerate(true);
+      setShowInput(false);
     }, 1000);
   };
 
@@ -38,6 +40,7 @@ const LinkedToolEffect = () => {
         setHasCompleted(false);
         setTrigger(true);
         setShowGenerate(false);
+        setShowInput(true);
       }
       if (prev === 0) {
         setTriggerAns(false);
@@ -87,46 +90,50 @@ const LinkedToolEffect = () => {
     <div ref={scope} className=" w-full">
       <div className="max-w-2xl mx-auto p-6">
         {/* Input section */}
-        <div className="flex flex-col items-center gap-3 mb-6 bg-neutral-900 p-4 rounded-lg h-28">
-          <div className="relative flex-1 h-40 bg-neutral-800 rounded overflow-hidden p-3 w-full">
+        {showInput && (
+          <div className="flex flex-col items-center gap-3 mb-6 bg-neutral-900 p-4 rounded-lg h-36">
+            <div className="relative flex-1 h-40 bg-neutral-800 rounded overflow-hidden p-3 w-full">
+              <TextEffect
+                per="char"
+                variants={blurSlideVariants}
+                trigger={trigger}
+                onAnimationComplete={handleQuestionComplete}
+              >
+                Animate your ideas with motion-primitives
+              </TextEffect>
+            </div>
+
+            <motion.div
+              className="enter-button px-4 py-1 bg-neutral-700 rounded text-sm "
+              layout
+              initial={{ width: 'auto' }}
+              animate={{ width: 'auto' }}
+              transition={{ duration: 0.2, ease: 'easeInOut' }}
+            >
+              {showGenerate ? (
+                <TextShimmer className="font-mono text-sm" duration={1}>
+                  Generating code...
+                </TextShimmer>
+              ) : (
+                <span>Enter ↵</span>
+              )}
+            </motion.div>
+          </div>
+        )}
+        {!showInput && (
+          <div className="   bg-neutral-800 rounded h-full p-3 w-full min-h-60 ">
             <TextEffect
               per="char"
-              variants={blurSlideVariants}
-              trigger={trigger}
-              onAnimationComplete={handleQuestionComplete}
+              preset="fade"
+              trigger={triggerAns}
+              onAnimationComplete={handleComplete}
             >
-              Animate your ideas with motion-primitives
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.taque
+              dolore recusandae, velit reprehenderit non asperiores hic minus?
+              Debitis.
             </TextEffect>
           </div>
-
-          <motion.div
-            className="enter-button px-4 py-1 bg-neutral-700 rounded text-sm "
-            layout
-            initial={{ width: 'auto' }}
-            animate={{ width: 'auto' }}
-            transition={{ duration: 0.2, ease: 'easeInOut' }}
-          >
-            {showGenerate ? (
-              <TextShimmer className="font-mono text-sm" duration={1}>
-                Generating code...
-              </TextShimmer>
-            ) : (
-              <span>Enter ↵</span>
-            )}
-          </motion.div>
-        </div>
-        <div className="   bg-neutral-800 rounded h-full p-3 w-full min-h-60 ">
-          <TextEffect
-            per="char"
-            preset="fade"
-            trigger={triggerAns}
-            onAnimationComplete={handleComplete}
-          >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.taque
-            dolore recusandae, velit reprehenderit non asperiores hic minus?
-            Debitis.
-          </TextEffect>
-        </div>
+        )}
       </div>
     </div>
   );
